1f365f8128cfd15eb0b14eaac4a6630c3a418c3b
Anders Mårtensson <Anders Mrtensson>
2016-05-09 09:33:55 +0200

    Removed Android specific Code to prepare for branch AndroidV3

--- android/src/com/main/AndroidLauncher.java
+++ android/src/com/main/AndroidLauncher.java
@@ -7,13 +7,8 @@ import android.os.Bundle;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
-import com.google.api.client.googleapis.extensions.android.gms.auth.GoogleAccountCredential;
-import com.google.api.client.util.ExponentialBackOff;
 import com.google.api.services.calendar.CalendarScopes;
 
-import java.util.Arrays;
-
-import data.Statics;
 import view.MainView;
 
 public class AndroidLauncher extends AndroidApplication {
@@ -43,84 +38,10 @@ public class AndroidLauncher extends AndroidApplication {
 			System.out.println("Permission denied");
 			Gdx.app.exit();
 		}
-
-//		try {
-//			Context con = getApplicationContext();
-//			//con.startActivity();
-//			// Initialize credentials and service object.
-//			GoogleAccountCredential mCredential = GoogleAccountCredential.usingOAuth2(
-//					context, Arrays.asList(SCOPES))
-//					.setBackOff(new ExponentialBackOff());
-//			con.getApplicationContext();
-//			System.out.println("Context fetched");
-//			//DownloadController dc = new DownloadController(mCredential);
-//		} catch (IOException e) {
-//			e.printStackTrace();
-//		}
-		try{
-			String accountName = getPreferences(Context.MODE_PRIVATE)
-					.getString(PREF_ACCOUNT_NAME, null);
-
-			Statics.GoogleCredential = GoogleAccountCredential.usingOAuth2(
-					context, Arrays.asList(SCOPES))
-					.setBackOff(new ExponentialBackOff());
-			//startActivityForResult(Statics.GoogleCredential.newChooseAccountIntent(),REQUEST_ACCOUNT_PICKER);
-			//Statics.GoogleCredential.newChooseAccountIntent();
-
-			//Thread.sleep(500);
-			//Statics.GoogleCredential = GoogleAccountCredential.usingOAuth2()
-			//isGooglePlayServicesAvailable();
-			//Statics.GoogleCredential = mCredential;
-		}
-		catch (Exception e){
-			System.out.println("Failed getting credentials");
-			System.exit(0);
-		}
+        
 
 		initialize(new MainView(true), config);
 
-		// ATTENTION: This was auto-generated to implement the App Indexing API.
-		// See https://g.co/AppIndexing/AndroidStudio for more information.
-		//client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
 	}
 
-//	@Override
-//	public void onStart() {
-//		super.onStart();
-//
-//		// ATTENTION: This was auto-generated to implement the App Indexing API.
-//		// See https://g.co/AppIndexing/AndroidStudio for more information.
-//		client.connect();
-//		Action viewAction = Action.newAction(
-//				Action.TYPE_VIEW, // TODO: choose an action type.
-//				"AndroidLauncher Page", // TODO: Define a title for the content shown.
-//				// TODO: If you have web page content that matches this app activity's content,
-//				// make sure this auto-generated web page URL is correct.
-//				// Otherwise, set the URL to null.
-//				Uri.parse("http://host/path"),
-//				// TODO: Make sure this auto-generated app deep link URI is correct.
-//				Uri.parse("android-app://com.main/http/host/path")
-//		);
-//		AppIndex.AppIndexApi.start(client, viewAction);
-//	}
-//
-//	@Override
-//	public void onStop() {
-//		super.onStop();
-//
-//		// ATTENTION: This was auto-generated to implement the App Indexing API.
-//		// See https://g.co/AppIndexing/AndroidStudio for more information.
-//		Action viewAction = Action.newAction(
-//				Action.TYPE_VIEW, // TODO: choose an action type.
-//				"AndroidLauncher Page", // TODO: Define a title for the content shown.
-//				// TODO: If you have web page content that matches this app activity's content,
-//				// make sure this auto-generated web page URL is correct.
-//				// Otherwise, set the URL to null.
-//				Uri.parse("http://host/path"),
-//				// TODO: Make sure this auto-generated app deep link URI is correct.
-//				Uri.parse("android-app://com.main/http/host/path")
-//		);
-//		AppIndex.AppIndexApi.end(client, viewAction);
-//		client.disconnect();
-//	}
 }
